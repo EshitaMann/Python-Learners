416506eaa7eb6dedfa6bd5951bcb6d1d7ed4f1f2
E_M <eshita.mann100@gmail.com>
2017-10-09 18:50:25 +0530

    Add files via upload

---
+++ greatest.py
@@ -0,0 +1,7 @@
+#To find greatest of two no.s
+a=float(input("Enter 1st no:"))                                               
+b=float(input("Enter 2nd no:"))                                               
+if a>b:
+    print('1st no. is greater')                                               
+else:
+    print('2nd no. is greater')
