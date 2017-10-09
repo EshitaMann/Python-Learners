e41d3ed9da83cbe535e2034ba1e260dd0832bda0
E_M <eshita.mann100@gmail.com>
2017-10-09 19:02:09 +0530

    Add files via upload

---
+++ positivenegative.py
@@ -0,0 +1,6 @@
+#To find wether the no. is positive or negative
+i=int(input("Enter the no:"))                                                                                                                                          
+if i<0 :
+      print('No.is negative')                                                 
+else:
+      print('No.is positive')  
